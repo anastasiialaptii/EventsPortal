6e3b3f0316d1b25ce0115180f84dfb051b84c1e0
Anastasiia Laptii <laptiyanastasia@gmail.com>
2020-07-29 16:41:17 +0300

    fix pagination, add create-event service

--- EventsPortalClient/src/app/public-event-list/public-event-list.component.ts
+++ EventsPortalClient/src/app/public-event-list/public-event-list.component.ts
@@ -5,6 +5,7 @@ import { VisitService } from '../shared/services/visit-service';
 import { UserService } from '../shared/services/user-service';
 import { Visit } from '../shared/models/visit-model';
 import { EventItem } from '../shared/models/event-model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-public-event-list',
@@ -15,29 +16,82 @@ import { EventItem } from '../shared/models/event-model';
 
 export class PublicEventListComponent implements OnInit {
   token = JSON.parse(localStorage.getItem('socialusers'));
+  userId: number;
   visit: Visit;
-
+  tableMode: boolean = true;
   itemsEvent = [];
+  eventType = [
+    { Id: 1, Name: "Private" },
+    { Id: 2, Name: "Public" }
+  ];
   eventItem: EventItem[];
   pageOfItemsEvent: Array<EventItem>;
+  event: EventItem = new EventItem();
 
   constructor(
     public eventService: EventService,
     public visitService: VisitService,
     public userService: UserService
-    ) { }
+  ) { }
 
   ngOnInit(): void {
-   this.eventService.GetPublicEventList().subscribe((res: any)=>{
-     this.eventItem = res;
-   debugger;
-   console.log(res)
-   this.itemsEvent = Array(20).fill(0).map((x,i) => ({data:this.eventItem[i]}));
-  });
+    this.resetForm();
+    this.eventService.GetPublicEventList().subscribe((res: any) => {
+      this.eventItem = res;
+      debugger;
+      console.log(res)
+      this.itemsEvent = Array(this.eventItem.length).fill(0).map((x, i) => ({ data: this.eventItem[i] }));
+    });
   }
 
-  onChangePage1(pageOfItemsEvent: Array<any>) {
+  onChangePage(pageOfItemsEvent: Array<any>) {
     this.pageOfItemsEvent = pageOfItemsEvent;
+  }
+
+  onSubmit(form: NgForm) {
+    if (this.eventService.formData.Id == 0) {
+      this.createEvent(form);
+    }
+  }
+
+  cancel() {
+    this.event = new EventItem();
+    this.tableMode = true;
+  }
+
+  add() {
+    this.cancel();
+    this.tableMode = false;
+  }
+  resetForm(form?: NgForm) {
+    if (form != null)
+      form.form.reset();
+
+    this.userService.GetUserByToken(this.token.Message).subscribe(
+      res => {
+        this.eventService.formData = {
+          Id: 0,
+          Name: '',
+          Location: '',
+          Description: '',
+          ImageURI: '',
+          EventTypeId: 1,
+          OrganizerId: res
+        }
+      }
+    )
+  }
+
+  createEvent(form: NgForm) {
+    this.eventService.CreateEvent().subscribe(
+      res => {
+        this.eventService.GetPrivateEventList(this.token.Message);
+        this.resetForm();
+      },
+      err => {
+        debugger;
+        console.log(err);
+      })
   }
 
   createVisit(id: number) {
