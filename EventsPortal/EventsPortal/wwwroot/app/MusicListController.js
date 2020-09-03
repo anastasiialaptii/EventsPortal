21c4c690bd55f199dda97298cc35036877766523
Anastasiia <anastasiia.laptii@tech-stack.io>
2020-09-03 17:43:20 +0300

    add angularjs to an app

---
+++ EventsPortal/EventsPortal/wwwroot/app/MusicListController.js
@@ -0,0 +1,15 @@
+﻿(function (app) {
+    var MusicListController = function ($scope, $http) {
+        $scope.message = "hiya";
+        $http({
+            method: 'GET',
+            url: 'http://localhost:50618/api/Event/GetPublicEvents'
+        }).then(function (response) {
+            console.dir(response);
+            $scope.musics = response.data;
+        }, function (error) {
+            console.log(error);
+        });
+    };
+    app.controller("MusicListController", MusicListController);
+}(angular.module("theMusic", [])));
